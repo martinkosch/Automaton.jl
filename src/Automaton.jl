2f2bbff73e71b4cfaf058a5c95e2fea69cb93157
Martin Kosch <martin.kosch@rwth-aachen.de>
2020-04-03 21:37:56 +0200

    Git repair

--- src/Automaton.jl
+++ src/Automaton.jl
@@ -2,7 +2,7 @@ module Automaton
 
 using OrderedCollections
 
-# export StateMachine
+export State, Transition, StateMachine
 
 abstract type AbstractState end
 abstract type AbstractTransition end
@@ -14,30 +14,48 @@ const FunctionSym = Symbol
 mutable struct StateCallback
     enter_state::OrderedDict{}
     leave_state::OrderedDict{}
+    function StateCallback(enter_state::OrderedDict{}=OrderedDict(),
+        leave_state::OrderedDict{}=OrderedDict())
+        return new(enter_state, leave_state)
+    end
 end
 
 mutable struct TransitionCallback
     start_transition::OrderedDict{}
     end_transition::OrderedDict{}
+    function TransitionCallback(start_transition::OrderedDict{}=OrderedDict(),
+        end_transition::OrderedDict{}=OrderedDict())
+        return new(start_transition, end_transition)
+    end
 end
 
 mutable struct StateMachineCallback
-    initial::OrderedDict{}
-    final::OrderedDict{}
     start_any_transition::OrderedDict{}
     end_any_transition::OrderedDict{}
     enter_any_state::OrderedDict{}
-    leave_any_state::OrderedDict{FunctionSym, }
+    leave_any_state::OrderedDict{}
+    function TransitionCallback(start_any_transition::OrderedDict{}=OrderedDict(),
+        end_any_transition::OrderedDict{}=OrderedDict(),
+        enter_any_state::OrderedDict{}=OrderedDict(),
+        leave_any_state::OrderedDict{}=OrderedDict())
+        return new(start_any_transition, end_any_transition, enter_any_state, leave_any_state)
+    end
 end
 
-struct State <: AbstractState
+mutable struct State <: AbstractState
     callbacks::StateCallback
+    function State(callbacks::StateCallback=StateCallback())
+        return new(callbacks)
+    end
 end
 
-struct Transition <: AbstractTransition
+mutable struct Transition <: AbstractTransition
     from::StateSym
     to::StateSym
     callbacks::TransitionCallback
+    function Transition(from::StateSym, to::StateSym, callbacks::TransitionCallback=TransitionCallback())
+        return new(from, to, callbacks)
+    end
 end
 
 mutable struct StateMachine
@@ -45,10 +63,9 @@ mutable struct StateMachine
     transitions::Dict{TransitionSym,<:AbstractTransition}
     callbacks::StateMachineCallback
     current::Union{StateSym, Nothing}
-    terminal::Union{StateSym, Nothing}
     function StateMachine(states::Dict{StateSym,<:AbstractState},
         transitions::Dict{TransitionSym,<:AbstractTransition},
-        callbacks::StateMachineCallback,
+        callbacks::StateMachineCallback=StateMachineCallback(),
         initial::Union{StateSym, Nothing}=nothing,
         terminal::Union{StateSym, Nothing}=nothing)
         return new(states, transitions, callbacks, inital, terminal)
